0d7c1838c793e48270f7c29b637b7128d8cd9308
zhao520wei <zhao520wei1@163.com>
2020-07-30 11:16:22 +0800

    custom cell

--- wfchat/WildFireChat/WFChatUIKit/MessageList/Cell/WFCULeaveCell.m
+++ wfchat/WildFireChat/WFChatUIKit/MessageList/Cell/WFCULeaveCell.m
@@ -13,6 +13,10 @@
 
 @property (nonatomic, strong)UILabel *titleLabel;
 
+@property (nonatomic, strong)UILabel *contentLabel;
+
+@property (nonatomic, strong)UILabel * bottomLabel;
+
 @end
 
 @implementation WFCULeaveCell
@@ -20,7 +24,7 @@
 + (CGSize)sizeForClientArea:(WFCUMessageModel *)msgModel withViewWidth:(CGFloat)width {
     WFCCLeaveMessageContent *imgContent = (WFCCLeaveMessageContent *)msgModel.message.content;
     
-    CGSize size = CGSizeMake(100, 100);
+    CGSize size = CGSizeMake(150, 130);
     
     if (size.height > width || size.width > width) {
         float scale = MIN(width/size.height, width/size.width);
@@ -33,21 +37,48 @@
     [super setModel:model];
     
     WFCCLeaveMessageContent *leaveContent = (WFCCLeaveMessageContent *)model.message.content;
-    self.titleLabel.text = [NSString stringWithFormat:@"%@ -- %@",leaveContent.title,leaveContent.reason];
+    self.titleLabel.text = [NSString stringWithFormat:@"  %@",leaveContent.title];
+    self.contentLabel.text = [NSString stringWithFormat:@"  请假事由: %@\n  开始时间： %lld\n  结束时间: %lld",leaveContent.reason,leaveContent.startTime,leaveContent.endTime];
+
+    self.bottomLabel.text = @"  查看详情";
 }
 
 
 
 - (UILabel *)titleLabel {
     if (!_titleLabel) {
-        _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.bubbleView.frame.size.width, 40)];
-        _titleLabel.font = [UIFont systemFontOfSize:12];
-        _titleLabel.textAlignment = NSTextAlignmentCenter;
+        _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.bubbleView.frame.size.width, 30)];
+        _titleLabel.font = [UIFont systemFontOfSize:16];
+        _titleLabel.textAlignment = NSTextAlignmentLeft;
         _titleLabel.backgroundColor = [UIColor colorWithRed:0.7f green:0.7f blue:0.7f alpha:0.5f];
-        _titleLabel.numberOfLines = 0;
+        _titleLabel.numberOfLines = 1;
         [self.bubbleView addSubview:_titleLabel];
     }
     return _titleLabel;
+}
+
+- (UILabel *)contentLabel{
+    if (!_contentLabel) {
+        _contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 30 + 10, self.bubbleView.frame.size.width, self.bubbleView.frame.size.height - 80)];
+        _contentLabel.font = [UIFont systemFontOfSize:14];
+        _contentLabel.textAlignment = NSTextAlignmentLeft;
+        _contentLabel.backgroundColor = [UIColor clearColor];
+        _contentLabel.numberOfLines = 0;
+        [self.bubbleView addSubview:_contentLabel];
+    }
+    return _contentLabel;
+}
+-(UILabel *)bottomLabel{
+    if (!_bottomLabel) {
+        _bottomLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.bubbleView.frame.size.height - 30, self.bubbleView.frame.size.width, 30)];
+        _bottomLabel.font = [UIFont systemFontOfSize:14];
+        _bottomLabel.textAlignment = NSTextAlignmentLeft;
+        _bottomLabel.backgroundColor = [UIColor colorWithRed:0.7f green:0.7f blue:0.7f alpha:0.5f];
+        _bottomLabel.numberOfLines = 1;
+        _bottomLabel.textColor  = kMainColor;
+        [self.bubbleView addSubview:_bottomLabel];
+    }
+    return _bottomLabel;;
 }
 
 
