d13cf73d426a02ebbfa4f46db2cb828b7030c9df
zhao520wei <zhao520wei1@163.com>
2020-09-24 15:11:22 +0800

    GIF 图片

--- wfchat/WildFireChat/WFChatUIKit/MessageList/Cell/WFCUStickerCell.m
+++ wfchat/WildFireChat/WFChatUIKit/MessageList/Cell/WFCUStickerCell.m
@@ -8,11 +8,18 @@
 
 #import "WFCUStickerCell.h"
 #import <WFChatClient/WFCChatClient.h>
+#import "MBProgressHUD.h"
+#import "YLImageView.h"
+#import "YLGIFImage.h"
 
+#import "SDFLAnimatedImage.h"
+#import "FLAnimatedImageView+WebCache.h"
+#import "FLAnimatedImageView.h"
+#import "FLAnimatedImage.h"
 #import "WFCUMediaMessageDownloader.h"
 #import "UIImageView+WebCache.h"
 @interface WFCUStickerCell ()
-@property (nonatomic, strong)UIImageView *thumbnailView;
+@property (nonatomic, strong)FLAnimatedImageView *thumbnailView;
 @end
 
 @implementation WFCUStickerCell
@@ -32,9 +39,16 @@
     [super setModel:model];
     
     WFCCStickerMessageContent *stickerMsg = (WFCCStickerMessageContent *)model.message.content;
+   
     __weak typeof(self) weakSelf = self;
     if (!stickerMsg.localPath.length) {
+//        model.mediaDownloading = YES;
+        NSLog(@"开始下载 sticker ");
+//        __block MBProgressHUD * hud = [MBProgressHUD showHUDAddedTo:self animated:YES];
         BOOL downloading = [[WFCUMediaMessageDownloader sharedDownloader] tryDownload:model.message success:^(long long messageUid, NSString *localPath) {
+//             model.mediaDownloading = NO;
+            NSLog(@" sticker  下载完成");
+//            [hud hideAnimated:YES];
             if (messageUid == weakSelf.model.message.messageUid) {
                 weakSelf.model.mediaDownloading = NO;
                 stickerMsg.localPath = localPath;
@@ -44,24 +58,58 @@
             if (messageUid == weakSelf.model.message.messageUid) {
                 weakSelf.model.mediaDownloading = NO;
             }
+//            hud.mode = MBProgressHUDModeText;
+//            hud.label.text = [NSString stringWithFormat:@"下载Stiker失败 error_code: %d",error_code];
+//            [hud hideAnimated:YES afterDelay:1];
+            NSLog(@" sticker  下载失败");
         }];
         if (downloading) {
             model.mediaDownloading = YES;
         }
     }
-    
+  
     self.thumbnailView.frame = self.bubbleView.bounds;
+    
+     /*
     if (stickerMsg.localPath.length) {
-        [self.thumbnailView sd_setImageWithURL:[NSURL fileURLWithPath:stickerMsg.localPath]];
+        NSLog(@"已有 sticker  ");
+//        if (@available(iOS 14, *)) {
+            [self.thumbnailView sd_setImageWithURL:[NSURL fileURLWithPath:stickerMsg.localPath]];
+            NSLog(@" -----  14");
+//        } else {
+//            NSLog(@" -----  13");
+//            self.thumbnailView.image = [YLGIFImage imageWithContentsOfFile:stickerMsg.localPath];
+//        }
+        
     } else {
         self.thumbnailView.image = nil;
     }
+      */
+    
+//    UIImage * image = [UIImage imageNamed:@"default_gif"];
+//    NSData * gifData =  UIImagePNGRepresentation(image);
+//    FLAnimatedImage *animatedImage = [FLAnimatedImage animatedImageWithGIFData:gifData];
+//    SDFLAnimatedImage *placeholder = [[SDFLAnimatedImage alloc] initWithAnimatedImage:animatedImage];
+    if (stickerMsg.localPath.length) {
+        [self.thumbnailView sd_setImageWithURL:[NSURL fileURLWithPath:stickerMsg.localPath] placeholderImage:nil];
+    } else {
+        [self.thumbnailView sd_setImageWithURL:[NSURL URLWithString:stickerMsg.remoteUrl] placeholderImage:nil];
+    }
+    
+
+
     self.bubbleView.image = nil;
 }
 
-- (UIImageView *)thumbnailView {
+- (FLAnimatedImageView *)thumbnailView {
     if (!_thumbnailView) {
-        _thumbnailView = [[UIImageView alloc] init];
+//        if (@available(iOS 14, *)) {
+//            _thumbnailView = [[UIImageView alloc] init];
+//        } else {
+//            _thumbnailView = [[YLImageView alloc] init];
+//        }
+        _thumbnailView = [[FLAnimatedImageView alloc] init];
+        _thumbnailView.contentMode = UIViewContentModeScaleAspectFit;
         [self.bubbleView addSubview:_thumbnailView];
     }
     return _thumbnailView;
